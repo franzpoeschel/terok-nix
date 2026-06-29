eebf835b1e1318fbab5473a49d6d41e847cfd1f9
Franz Pöschel <franz.poeschel@gmail.com>
2026-06-29 14:43:36 +0200

    Formatting

--- terok-overlay.nix
+++ terok-overlay.nix
@@ -13,7 +13,7 @@ let
       pythonPackagesOverlays = (prev.pythonPackagesOverlays or [ ]) ++ [
         (python-final: python-prev: {
           asyncvarlink = callPackage ./asyncvarlink { };
-          terok = python-final.toPythonModule  final.terok;
+          terok = python-final.toPythonModule final.terok;
           terok-clearance = callPackage ./terok-clearance { };
           terok-executor = callPackage ./terok-executor { };
           terok-sandbox = callPackage ./terok-sandbox { };
