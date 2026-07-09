4a63ed2de9c3c41004bd43d2505930539ea6ec84
Franz Pöschel <franz.poeschel@gmail.com>
2026-07-09 16:04:29 +0200

    edit-packaging shell as the default

--- devShells/default.nix
+++ devShells/default.nix
@@ -1,18 +1,22 @@
 { pkgs, checks }:
-{
-  edit-packaging =
-    let
-      inherit (checks.pre-commit-check) shellHook enabledPackages;
-    in
-    pkgs.mkShell {
-      nativeBuildInputs =
-        with pkgs;
-        [
-          git
-          pre-commit
-          nixfmt
-        ]
-        ++ enabledPackages;
-      inherit shellHook;
-    };
-}
+let
+  shells = {
+    default = shells.edit-packaging;
+    edit-packaging =
+      let
+        inherit (checks.pre-commit-check) shellHook enabledPackages;
+      in
+      pkgs.mkShell {
+        nativeBuildInputs =
+          with pkgs;
+          [
+            git
+            pre-commit
+            nixfmt
+          ]
+          ++ enabledPackages;
+        inherit shellHook;
+      };
+  };
+in
+shells
